0ed6ca1a491c91bede55e3c04c5eda118cea78f2
Mike Falk <falk478@yahoo.com>
2021-02-19 18:15:23 -0800

    added some stuff tried to create a get meals page got stuck on handling lists in views.

---
+++ ECommerce-App/Models/MealsListVm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce_App.Models
+{
+    public class MealsListVm
+    {
+        public List<Meal> ListOfMeals { get; set; }
+       
+        public Meal Id { get; set; }
+        public Meal Name { get; set; }
+
+        public Meal Ingredients { get; set; }
+
+        public Meal Price { get; set; }
+
+        public Meal Nutrition { get; set; }
+
+        public Meal Type { get; set; }
+
+        public Category Category { get; set; }
+        
+       
+    }
+}
